35ba5f03b781a03765458d9f10b8f86580c7c0df
Ryan Kendrick <rpkendrick@gmail.com>
2018-10-24 17:55:26 +0000

    completed character positioning activity with the function returning array of indices rather than string

--- character-position.js
+++ character-position.js
@@ -2,20 +2,17 @@ var args = process.argv.slice(2);
 
 // function takes a string, joins the words of the string and sets all characters
 // to lower case, returns an object with individual characters in the given string
-// set as keys, and the key values set as how many instances that character
-// appears in the entire string.
+// set as keys, and the key values set as the indices of each character
+// in the entire string.
 function countLetters(string) {
   string = string.join('').toLowerCase();
   var charCount = {};
-  charCount[string[0]] = 1;
-  var key = string[0];
-  for (var i = 1; i < string.length; i++) {
+  for (var i = 0; i < string.length; i++) {
     var letter = string[i];
-    if (key.includes(letter)) {
-     charCount[letter]++;
+    if (charCount[letter]) {
+     charCount[letter].push(i);
     } else {
-      charCount[letter] = 1;
-      key += letter;
+      charCount[letter] = [i];
     }
   }
   return charCount;
